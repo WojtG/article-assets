fc549b1f1625d9835c92668c8078da00a58f95e8
WojtG <wojtekgg431@gmail.com>
2023-11-13 13:24:08 +0100

    5

---
+++ NRWL Extension/snippet3.js
@@ -0,0 +1,16 @@
+yargs.command(
+  "init",
+  "Initialize new workspace",
+  {
+    name: {
+      alias: "n",
+      type: "string",
+      demandOption: true,
+    },
+  },
+  async (args) => {
+    await workspaceInit({
+      name: args.name,
+    });
+  }
+);
